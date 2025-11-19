2ab80f30222795f6b1e7640d102972243ce8b574
melvinkest <melvinkestmagaia71@gmail.com>
2025-11-19 14:13:28 +0400

    Update 05 Days of the Month - Advanced.py

--- Exercise 05 - Days of the Month/05 Days of the Month - Advanced.py
+++ Exercise 05 - Days of the Month/05 Days of the Month - Advanced.py
@@ -1,15 +1,16 @@
+#Setting the list
 days= {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
 month= int(input('Enter a month number (1-12):'))
 if month in days:
     if month == 2: 
-            # Ask the user if the year is a leap year
+            # Asking the user if the year is a leap year
             leap = input("Is the year a leap year? (yes/no): ").strip().lower()
             
-            # Check the user's response
+            # Checking the user's response
             if leap == 'yes':
-                days[2] = 29  # Adjust February's days to 29
+                days[2] = 29  # Adjusting February's days to 29
             elif leap != 'no':
-                 # Handle input that isn't clearly 'yes' or 'no'
+                 # Handling input that isn't clearly 'yes' or 'no'
                 print("Invalid response for leap year status. Assuming non-leap year (28 days).")
     days = days[month]
     print(f"Month {month} has {days} days.")
